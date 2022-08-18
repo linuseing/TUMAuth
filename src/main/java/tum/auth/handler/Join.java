a82f07db2dc3023ec3027fc077bdee5a22cd5e33
linuseing <linus.eing@tum.de>
2022-08-18 10:58:13 +0200

    authenticate player on join

--- src/main/java/tum/auth/handler/Join.java
+++ src/main/java/tum/auth/handler/Join.java
@@ -1,25 +1,41 @@
 package tum.auth.handler;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import tum.auth.TumAuth;
+import tum.auth.datasource.DataSource;
+import tum.auth.models.User;
 
 import java.util.UUID;
 
 public class Join implements Listener {
 
-    private TumAuth tumAuth;
+    private final TumAuth tumAuth;
+    private final DataSource dataSource;
 
     public Join(TumAuth tumAuth) {
         this.tumAuth = tumAuth;
+        this.dataSource = tumAuth.getDataSource();
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        UUID uuid = event.getPlayer().getUniqueId();
-
-
-        event.getPlayer().kickPlayer("Please register at: 'https://duhund.com'");
+        Player player = event.getPlayer();
+        UUID uuid = player.getUniqueId();
+        try {
+            User user = dataSource.getUser(uuid);
+            if (user == null) {
+                player.chat("Please set your TUM-ID using /tumid <id>");
+                //TODO: freeze
+                return;
+            }
+            if (user.isAuthenticated())
+                return;
+            player.kickPlayer("Please activate your Token to be able to access this server.");
+        } catch (Exception e) {
+            player.kickPlayer("Some error occurred reaching the database. Please try again later or contact an admin.");
+        }
     }
 }
